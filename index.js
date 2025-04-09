28d3a902180541199ca10c0f0cd7b057d6c64379
token221 <tokenxxxx9@gmail.com>
2025-04-09 19:21:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to PARDOSS: http://localhost:${port}`);
+  console.log(`🔗 Powered By PARDOSS`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Habibat FURY","A7777"];
 
 
 let currentIndex = 0;
